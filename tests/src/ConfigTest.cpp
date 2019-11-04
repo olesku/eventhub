fab01db4376d0a219fed8fcc13a564ad7993ad8b
Ole Fredrik Skudsvik <ole.skudsvik@gmail.com>
2019-11-04 21:10:41 +0100

    Add some more test cases

--- tests/src/ConfigTest.cpp
+++ tests/src/ConfigTest.cpp
@@ -5,15 +5,26 @@
 using namespace eventhub;
 using namespace std;
 
-TEST_CASE("Test config class", "[config]") {
-  GIVEN("That we set a integer env variable.") {
-    THEN("We should be able to retrieve it though our config object.") {
-      //Config.addOption<int>("LISTEN_PORT", 8080);
-      //REQUIRE(Config.get<int>("LISTEN_PORT") == 8080);
+TEST_CASE("Integer configuration param test" "[config]") {
+  GIVEN("That we set a default value") {
+    Config.addOption<int>("MY_INT_PARAM_1", 1000);
+    THEN("We should get that value back if the env is unset.") {
+      REQUIRE(Config.get<int>("MY_INT_PARAM_1") == 1000);
+    }
+  }
 
-      setenv("LISTEN_PORT", "9090", 1);
-      Config.addOption<int>("LISTEN_PORT", 8080);
-      REQUIRE(Config.get<int>("LISTEN_PORT") == 9090);
+  GIVEN("That the environment has a variable set") {
+    setenv("MY_INT_PARAM_2", "2000", 1);
+    Config.addOption<int>("MY_INT_PARAM_2", 1000);
+    THEN("We should get the value of that env and not the default value") {
+      REQUIRE(Config.get<int>("MY_INT_PARAM_2") == 2000);
+    }
+  }
+
+  GIVEN("That a integer config env has a non numeric value") {
+    setenv("MY_INIT_PARAM_3", "BADVALUE", 1);
+    THEN("We should get an exception when we call addOption") {
+      CHECK_THROWS_AS(Config.addOption<int>("MY_INIT_PARAM_3", 1000), InvalidValue);
     }
   }
 }
