7ba262cf8dbe304c53455b7de81e679b67f03864
olesku <ole.skudsvik@gmail.com>
2021-12-21 13:02:47 +0100

    Set correct flag type for disable_unsecure_listener.

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char** argv) {
       { "ssl_private_key",           ConfigValueType::STRING, "",          ConfigValueSettings::OPTIONAL },
       { "ssl_cert_auto_reload",      ConfigValueType::BOOL,   "false",     ConfigValueSettings::OPTIONAL },
       { "ssl_cert_check_interval",   ConfigValueType::INT,    "300",       ConfigValueSettings::OPTIONAL },
-      { "disable_unsecure_listener", ConfigValueType::INT,    "300",       ConfigValueSettings::OPTIONAL }
+      { "disable_unsecure_listener", ConfigValueType::BOOL,   "false",     ConfigValueSettings::OPTIONAL }
     };
 
   Config cfg(cfgMap);
