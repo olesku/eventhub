4870f23fbcaff4be22068093e15bd35a31f55054
olesku <ole.skudsvik@gmail.com>
2022-06-15 17:34:43 +0200

    Ensure subject is not empty.

--- src/RPCHandler.cpp
+++ src/RPCHandler.cpp
@@ -247,10 +247,10 @@ void RPCHandler::_handlePublish(HandlerContext& ctx, jsonrpcpp::request_ptr req)
 
   try {
     auto& redis = ctx.server()->getRedis();
+    const auto& subject = accessController->subject();
 
-    if (accessController->getRateLimitConfig().hasLimits()) {
-      auto& subject = accessController->subject();
-      auto limits = accessController->getRateLimitConfig().getRateLimitConfigForTopic(topicName);
+    if (!subject.empty() && accessController->getRateLimitConfig().hasLimits()) {
+      const auto limits = accessController->getRateLimitConfig().getRateLimitConfigForTopic(topicName);
 
       if (redis.isRateLimited(limits.topic, subject, limits.max)) {
         LOG->trace("PUBLISH {}: User {} is currently ratelimited. Interval: {} Max: {} Matched ratelimit pattern: {}", topicName, subject, limits.interval, limits.max, limits.topic);
